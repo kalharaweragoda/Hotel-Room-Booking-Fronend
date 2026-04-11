cc515650f43732f3574801e3953c54fbed9ddb89
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-11 23:56:33 +0530

    register.component.ts file create

--- booking-app/src/app/register.component/register.component.ts
+++ booking-app/src/app/register.component/register.component.ts
@@ -1,9 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { ApiService } from '../service/api.service';
 
 @Component({
-  selector: 'app-register.component',
-  imports: [],
+  selector: 'app-register',
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css',
+  styleUrl: './register.component.css'
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+
+  constructor( private apiService: ApiService, private router: Router){}
+
+  formData: any = {
+
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    password: ''
+  }
+
+  error: any = null;
+
+  handleSubmit(){
+    if (
+      !this.formData.email ||
+      !this.formData.firstName ||
+      !this.formData.lastName ||
+      !this.formData.phoneNumber ||
+      !this.formData.password
+    ) {
+      this.showError('Please all fields are required');
+      return
+    }
+
+    this.apiService.registerUser(this.formData).subscribe({
+      next:(res: any) => {
+        this.router.navigate(['/login'])
+      },
+      error: (err: any) => {
+        this.showError(err?.error?.message || err.message || 'Unable to Register a user: ' + err)
+      }
+    });
+  }
+
+  showError(msg: string){
+    this.error = msg;
+    setTimeout(()=> {
+      this.error = null
+    }, 4000);
+  }
+}
