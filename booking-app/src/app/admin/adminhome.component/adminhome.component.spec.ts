8b5844f4539575c6e68aa9e1046e27784923ab91
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-14 23:34:55 +0530

    rename addroom componment

---
+++ booking-app/src/app/admin/adminhome.component/adminhome.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminhomeComponent } from './adminhome.component';
+
+describe('AdminhomeComponent', () => {
+  let component: AdminhomeComponent;
+  let fixture: ComponentFixture<AdminhomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AdminhomeComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AdminhomeComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ booking-app/src/app/admin/adminhome.component/adminhome.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminhome.component',
+  imports: [],
+  templateUrl: './adminhome.component.html',
+  styleUrl: './adminhome.component.css',
+})
+export class AdminhomeComponent {}
