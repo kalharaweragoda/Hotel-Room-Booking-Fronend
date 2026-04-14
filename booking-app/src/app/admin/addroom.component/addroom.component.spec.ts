88b1a011e8c73e0a8af9d4014a6ab605c966bd09
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-14 23:28:43 +0530

    create admin component

---
+++ booking-app/src/app/admin/addroom.component/addroom.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddroomComponent } from './addroom.component';
+
+describe('AddroomComponent', () => {
+  let component: AddroomComponent;
+  let fixture: ComponentFixture<AddroomComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AddroomComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AddroomComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ booking-app/src/app/admin/addroom.component/addroom.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addroom.component',
+  imports: [],
+  templateUrl: './addroom.component.html',
+  styleUrl: './addroom.component.css',
+})
+export class AddroomComponent {}
