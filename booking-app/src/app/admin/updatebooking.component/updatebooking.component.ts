a3abd270f7753ebf596f2a9a60ec5bf398614cea
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-16 00:07:08 +0530

    create updatebooking.html

---
+++ booking-app/src/app/admin/updatebooking.component/updatebooking.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-updatebooking.component',
+  imports: [],
+  templateUrl: './updatebooking.component.html',
+  styleUrl: './updatebooking.component.css',
+})
+export class UpdatebookingComponent {}
