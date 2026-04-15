dc61698aaaf9df8b1f005b72455ae4a5604fbb01
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-16 00:00:05 +0530

    rename managerooms

---
+++ booking-app/src/app/admin/managerooms/managerooms.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ManageroomsComponent } from './managerooms.component';
+
+describe('ManageroomsComponent', () => {
+  let component: ManageroomsComponent;
+  let fixture: ComponentFixture<ManageroomsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ManageroomsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ManageroomsComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ booking-app/src/app/admin/managerooms/managerooms.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-managerooms.component',
+  imports: [],
+  templateUrl: './managerooms.component.html',
+  styleUrl: './managerooms.component.css',
+})
+export class ManageroomsComponent {}
