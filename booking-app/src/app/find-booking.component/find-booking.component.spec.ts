ab04c84fa8fc3384c2bd89e71b8f0c35619c6ce2
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-18 21:54:39 +0530

    create find-booking.component.html

---
+++ booking-app/src/app/find-booking.component/find-booking.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FindBookingComponent } from './find-booking.component';
+
+describe('FindBookingComponent', () => {
+  let component: FindBookingComponent;
+  let fixture: ComponentFixture<FindBookingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FindBookingComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(FindBookingComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ booking-app/src/app/find-booking.component/find-booking.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-booking.component',
+  imports: [],
+  templateUrl: './find-booking.component.html',
+  styleUrl: './find-booking.component.css',
+})
+export class FindBookingComponent {}
