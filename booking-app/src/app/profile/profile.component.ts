ae357b7f9fbe4fe51ab208fa671b37151ff2199b
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-18 22:18:20 +0530

    create profile.component.ts

--- booking-app/src/app/profile/profile.component.ts
+++ booking-app/src/app/profile/profile.component.ts
@@ -1,9 +1,71 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../service/api.service';
 
 @Component({
-  selector: 'app-profile.component',
-  imports: [],
+  selector: 'app-profile',
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css',
+  styleUrl: './profile.component.css'
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+
+  user: any = null;
+  bookings: any[] = [];
+  error: any = null;
+
+  constructor(private apiService: ApiService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.fetchUserProfile();
+  }
+
+  // Fetch user profile and bookings
+  fetchUserProfile() {
+    this.apiService.myProfile().subscribe({
+      next: (response: any) => {
+        this.user = response.user;
+        // Fetch bookings after the user profile is fetched
+        this.apiService.myBooking().subscribe({
+          next: (bookingResponse: any) => {
+            this.bookings = bookingResponse.bookings;
+          },
+          error: (err) => {
+            this.showError(
+              err?.error?.message ||
+                err?.error ||
+                'Error getting my bookings: ' + err
+            );
+          },
+        });
+      },
+      error: (err) => {
+        this.showError(
+          err?.error?.message ||
+            err?.error ||
+            'Error getting my profile info: ' + err
+        );
+      },
+    });
+  }
+
+  // Handle errors
+  showError(msg: string) {
+    this.error = msg;
+    setTimeout(() => {
+      this.error = null;
+    }, 4000);
+  }
+
+  // Handle logout
+  handleLogout() {
+    this.apiService.logout();
+    this.router.navigate(['/home']);
+  }
+
+  // Navigate to edit profile page
+  handleEditProfile() {
+    this.router.navigate(['/edit-profile']);
+  }
+}
