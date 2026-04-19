c2063e97fbd7b1c4c03964e3f5a3fa1248ca03f8
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-19 21:22:10 +0530

    paymentfailure.component rename

---
+++ booking-app/src/app/payment/paymentpage.component/paymentpage.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaymentpageComponent } from './paymentpage.component';
+
+describe('PaymentpageComponent', () => {
+  let component: PaymentpageComponent;
+  let fixture: ComponentFixture<PaymentpageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PaymentpageComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PaymentpageComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ booking-app/src/app/payment/paymentpage.component/paymentpage.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paymentpage.component',
+  imports: [],
+  templateUrl: './paymentpage.component.html',
+  styleUrl: './paymentpage.component.css',
+})
+export class PaymentpageComponent {}
