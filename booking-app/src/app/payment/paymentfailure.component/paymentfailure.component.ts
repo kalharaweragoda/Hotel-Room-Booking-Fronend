f230417cebc1ef5854c6e8f14288cd7ed0d059ef
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-19 21:19:51 +0530

    paymentfailure.component.html create

---
+++ booking-app/src/app/payment/paymentfailure.component/paymentfailure.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paymentfailure.component',
+  imports: [],
+  templateUrl: './paymentfailure.component.html',
+  styleUrl: './paymentfailure.component.css',
+})
+export class PaymentfailureComponent {}
