a21cea64540c8c06b43d736340411a47b5e87852
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-21 19:40:55 +0530

    rooms.component.ts create

--- booking-app/src/app/rooms/rooms.component.ts
+++ booking-app/src/app/rooms/rooms.component.ts
@@ -1,9 +1,116 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PaginationComponent } from '../pagination/pagination.component';
+import { RoomsearchComponent } from "../roomsearch/roomsearch.component";
+import { RoomresultComponent } from '../roomresult/roomresult.component';
 
 @Component({
-  selector: 'app-rooms.component',
-  imports: [],
+  selector: 'app-rooms',
+  imports: [PaginationComponent,
+    RoomresultComponent,
+    RoomsearchComponent,
+    CommonModule, FormsModule, RoomsearchComponent],
   templateUrl: './rooms.component.html',
-  styleUrl: './rooms.component.css',
+  styleUrl: './rooms.component.css'
 })
-export class RoomsComponent {}
+export class RoomsComponent {
+
+  rooms: any[] = [];
+  filteredRooms: any[] = [];
+  roomTypes: string[] = [];
+  selectedRoomType: string = '';
+  currentPage: number = 1;
+  roomsPerPage: number = 8;
+  error: any = null;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit():void{
+
+    this.fetchRooms();
+    this.fetchRoomTypes();
+
+  }
+
+  showError(msg: string): void {
+    this.error = msg;
+    setTimeout(() => {
+      this.error = null;
+    }, 5000);
+  }
+
+  // Fetch all rooms from the API
+  fetchRooms() {
+    this.apiService.getAllRooms().subscribe({
+      next: (response: any) => {
+        this.rooms = response.rooms;
+        this.filteredRooms = response.rooms;
+      },
+      error: (err) => {
+        this.showError(err?.error?.message || 'Error fetching rooms:' + err);
+      },
+    });
+  }
+
+  // Fetch room types from the API
+  fetchRoomTypes() {
+    this.apiService.getRoomTypes().subscribe({
+      next: (types: string[]) => {
+        this.roomTypes = types;
+      },
+      error: (err) => {
+        this.showError(
+          err?.error?.message || 'Error fetching room Types:' + err
+        );
+      },
+    });
+  }
+
+
+    // Handle the search result passed from RoomsearchComponent
+    handleSearchResult(results: any[]) {
+      this.rooms = results;
+      this.filteredRooms = results;
+    }
+
+    // Handle changes to room type filter
+    handleRoomTypeChange(event: any) {
+      const selectedType = event.target.value;
+      this.selectedRoomType = selectedType;
+      this.filterRooms(selectedType);
+    }
+
+  // Filter rooms by type
+  filterRooms(type: string) {
+    if (type === '') {
+      this.filteredRooms = this.rooms;
+    } else {
+      this.filteredRooms = this.rooms.filter((room) => room.type === type);
+    }
+    this.currentPage = 1;
+  }
+
+
+  // Pagination logic
+  get indexOfLastRoom() {
+    return this.currentPage * this.roomsPerPage;
+  }
+
+  get indexOfFirstRoom() {
+    return this.indexOfLastRoom - this.roomsPerPage;
+  }
+
+  get currentRooms() {
+    return this.filteredRooms.slice(
+      this.indexOfFirstRoom,
+      this.indexOfLastRoom
+    );
+  }
+
+  // Pagination function to change page
+  paginate(pageNumber: number) {
+    this.currentPage = pageNumber;
+  }
+}
