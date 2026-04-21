847c4f9f67a35e1707bbc3f1c778a014648c40ab
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-21 19:54:00 +0530

    create guard.service.spec.ts

--- booking-app/src/app/service/guard.service.ts
+++ booking-app/src/app/service/guard.service.ts
@@ -1,6 +1,35 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Route, Router, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class GuardServiceSpecTs {}
+export class GuardService implements CanActivate {
+
+  constructor(private apiService: ApiService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const requireAdmin = route.data['requiresAdmin'] || false;
+
+    if(requireAdmin) {
+      if(this.apiService.isAdmin()) {
+        return true;
+      } else {
+        this.router.navigate(['/login'], {
+          queryParams: {return: state.url}
+        });
+        return false;
+      }
+    } else {
+      if(this.apiService.isAuthenticated()) {
+        return true;
+      } else {
+        this.router.navigate(['/login'], {
+          queryParams: {returnUrl: state.url}
+        });
+        return false;
+      }
+    }
+  }
+}
