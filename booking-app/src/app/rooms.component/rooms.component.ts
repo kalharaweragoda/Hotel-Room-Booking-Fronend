44b4d31ef96b5ad8b33b7b9a7c7552ff67475bba
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-21 19:39:13 +0530

    rooms.component.html create

---
+++ booking-app/src/app/rooms.component/rooms.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms.component',
+  imports: [],
+  templateUrl: './rooms.component.html',
+  styleUrl: './rooms.component.css',
+})
+export class RoomsComponent {}
